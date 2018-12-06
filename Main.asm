02c9065469f18931b6daba9f313dc34cffee7e81
Amit Verma <averma2022@utexas.edu>
2018-12-06 12:36:29 -0600

    Finished Start Codon

--- Main.asm
+++ Main.asm
@@ -21,6 +21,32 @@ search	LDI R0, holding	        ;read the new char at x4600
 	TRAP x21
 	AND R4, R4, #0		;store 0 at x4600
 	STI R4, holding
+	LD R1, char		;R1 has previous character
+	BRz searchA		;search for an a
+	LD R2, aye	        ;check if prev char was an A
+	ADD R3, R1, R2
+	BRz searchU		;search for an U
+	LD R2, you	        ;check if prev char was a U
+	ADD R3, R1, R2
+	BRz searchG		;search for a G
+	ST R4, char
+	BR search		;if it's invalid go back to search
+searchA LD R1, aye		;check if the char in R0 is an A
+	ADD R2, R0, R1
+	BRnp search		;if not then it goes back to search
+	ST R0, char		;store A to prev char spot
+	BR search		;go to search
+searchU	LD R1, you		;check if the char is a U
+	ADD R2, R0, R1		
+	ST R0, char		;Store current char to previous char
+	BR search		;go back to search
+searchG	LD R1, gee		;check for G
+	ADD R2, R0, R1	
+	ST R4, char		;store 0 to x4600
+	BRnp search             ;if not a G, go back to search		
+	ST R4, char		;store 0 to x4600 
+	LD R0, bar		;display pipe bar
+	OUT
 	HALT
 stack      .FILL x4000
 isr_loc    .FILL x2600
